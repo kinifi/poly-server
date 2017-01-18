04fe925a45b09bfd575fb91808b7745b82dce10e
kinifi <chrisfigueroa16@gmail.com>
2017-01-17 21:51:23 -0800

    hopefully did this correctly with polies db upgrade

--- db/migrate/20170118050723_add_read_me_to_poly.rb
+++ db/migrate/20170118050723_add_read_me_to_poly.rb
@@ -1,8 +1,10 @@
 class AddReadMeToPoly < ActiveRecord::Migration[5.0]
 
-  create_table :users do |t|
+  create_table :polies do |t|
       t.string :name
-      t.string :email
+      t.string :description
+      t.string :repotype
+      t.string :repourl
       t.string :author
       t.string :owner
       t.string :homepage
